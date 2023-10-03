7abc979a234a985bb8f46eecf2c72283597807f3
Ömer Melih Savaş <omermelihsavas@outlook.com>
2023-10-04 00:56:48 +0300

    start sidebar

---
+++ src/pages/not-found/index.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return (
+        <div className="bg-red-300 text-red-700 p-5 flex items-center justify-center">
+            Not Found
+        </div>
+    )
+}
