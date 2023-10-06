d8b0ec804dae5557d69904dfe39afb245d1a0f85
Ömer Melih Savaş <omermelihsavas@outlook.com>
2023-10-06 15:38:31 +0300

    finish sidebar menu

---
+++ src/layouts/main/sidebar/menu/send/index.jsx
@@ -0,0 +1,9 @@
+import Button from "~/components/button";
+
+export default function Send() {
+    return (
+        <div className="py-2 w-[85%]">
+            <Button size="large">Gönder</Button>
+        </div>
+    )
+}
